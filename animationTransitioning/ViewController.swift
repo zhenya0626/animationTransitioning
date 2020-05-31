99284dc0b580fd1e29fc63a65e7a3e737872937c
zhenya0626 <riana0626@yahoo.co.jp>
2020-06-01 00:32:00 +0900

    Initial Commit

---
+++ animationTransitioning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animationTransitioning
+//
+//  Created by abi01373 on 2020/06/01.
+//  Copyright © 2020 abi01373. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
